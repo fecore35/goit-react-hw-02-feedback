0aa72ec7067456f575b48961869d00c889523238
Evgenii Bryzgun <e.bryzgun@gmail.com>
2021-10-14 14:42:25 +0300

    Добавляю стили для Notification

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,7 +1,7 @@
 import PropTypes from "prop-types";
-
+import s from "./Notification.module.css";
 function Notification({ message }) {
-  return <p>{message}</p>;
+  return <p className={s.text}>{message}</p>;
 }
 
 Notification.propTypes = {
