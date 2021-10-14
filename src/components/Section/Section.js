3bcef5c265b9b78cb97078f2b16afe610d5be8ee
Evgenii Bryzgun <e.bryzgun@gmail.com>
2021-10-14 15:09:25 +0300

    Добавляю стили для Section

--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,9 +1,10 @@
 import PropTypes from "prop-types";
+import s from "./Section.module.css";
 
 function Section({ title, children }) {
   return (
     <>
-      {title && <h2>{title}</h2>}
+      {title && <h2 className={s.title}>{title}</h2>}
       {children}
     </>
   );
